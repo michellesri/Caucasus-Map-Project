5a3e3927b6578a020485b48f6f0ac35529a3dc66
Michael Freeman <mjfreem1@gmail.com>
2016-06-01 18:40:21 -0700

    about eedits and map issues

--- js/lang.js
+++ js/lang.js
@@ -1,30 +1,30 @@
 var russian = document.getElementById('Russian');
-var russiaBlurb = document.getElementById("russiaBlurb");
-var turkish = document.getElementById("Turkish");
-var turkeyBlurb = document.getElementById("turkeyBlurb");
-var persian = document.getElementById("Persian");
-var iranBlurb = document.getElementById("iranBlurb");
-var azeri = document.getElementById("Azeri");
-var azerBlurb = document.getElementById("azerBlurb");
-var georgian = document.getElementById("Georgian");
-var georgiaBlurb = document.getElementById("georgiaBlurb");
-var armenian = document.getElementById("Armenian");
-var armeniaBlurb = document.getElementById("armeniaBlurb");
-var chechen = document.getElementById("Chechen");
-var chechnyaBlurb = document.getElementById("chechnyaBlurb");
-var ossetian = document.getElementById("Ossetian");
-var ossetiaBlurb = document.getElementById("ossetiaBlurb");
-var laz = document.getElementById("Laz");
-var lazBlurb = document.getElementById("lazBlurb");
-var avar = document.getElementById("Avar");
-var avarBlurb = document.getElementById("avarBlurb");
-var abkhaz = document.getElementById("Abkhaz");
-var abkhaziaBlurb = document.getElementById("abkhaziaBlurb");
-var kurd = document.getElementById("Kurd");
-var kurdBlurb = document.getElementById("kurdBlurb");
+var russiaBlurb = document.getElementById('russiaBlurb');
+var turkish = document.getElementById('Turkish');
+var turkeyBlurb = document.getElementById('turkeyBlurb');
+var persian = document.getElementById('Persian');
+var iranBlurb = document.getElementById('iranBlurb');
+var azeri = document.getElementById('Azeri');
+var azerBlurb = document.getElementById('azerBlurb');
+var georgian = document.getElementById('Georgian');
+var georgiaBlurb = document.getElementById('georgiaBlurb');
+var armenian = document.getElementById('Armenian');
+var armeniaBlurb = document.getElementById('armeniaBlurb');
+var chechen = document.getElementById('Chechen');
+var chechnyaBlurb = document.getElementById('chechnyaBlurb');
+var ossetian = document.getElementById('Ossetian');
+var ossetiaBlurb = document.getElementById('ossetiaBlurb');
+var laz = document.getElementById('Laz');
+var lazBlurb = document.getElementById('lazBlurb');
+var avar = document.getElementById('Avar');
+var avarBlurb = document.getElementById('avarBlurb');
+var abkhaz = document.getElementById('Abkhaz');
+var abkhaziaBlurb = document.getElementById('abkhaziaBlurb');
+var kurd = document.getElementById('Kurd');
+var kurdBlurb = document.getElementById('kurdBlurb');
+var legend = document.getElementById('legend');
 
-var array = [[russian, russiaBlurb], [turkish, turkeyBlurb], [persian, iranBlurb], [azeri, azerBlurb], [georgian, georgiaBlurb], [armenian, armeniaBlurb], [chechen, chechnyaBlurb], [ossetian, ossetiaBlurb], [laz, lazBlurb], [avar, avarBlurb], [abkhaz, abkhaziaBlurb], [kurd, kurdBlurb]]
-
+var array = [[russian, russiaBlurb], [turkish, turkeyBlurb], [persian, iranBlurb], [azeri, azerBlurb], [georgian, georgiaBlurb], [armenian, armeniaBlurb], [chechen, chechnyaBlurb], [ossetian, ossetiaBlurb], [laz, lazBlurb], [avar, avarBlurb], [abkhaz, abkhaziaBlurb], [kurd, kurdBlurb]];
 
 function iOn(i) {
   return function() {
@@ -38,13 +38,27 @@ function iOff(i) {
   };
 }
 
+function legendOff() {
+  legend.style.display = 'none';
+}
+
+function legendOn() {
+  legend.style.display = 'block';
+}
+
 function mouseMovt() {
 
   for (var i = 0; i < array.length; i++) {
     var mouseOn = iOn(i);
-    array[i][0].addEventListener('mouseover', mouseOn);
+    array[i][0].addEventListener('mouseover', function() {
+      mouseOn();
+      legendOff();
+    });
     var mouseOff = iOff(i);
-    array[i][0].addEventListener('mouseout', mouseOff);
+    array[i][0].addEventListener('mouseout', function() {
+      mouseOff();
+      legendOn();
+    });
   }
 }
 
