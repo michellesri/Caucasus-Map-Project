f42f5cc21381a64b0164b4b066d224a6bbc219ba
Michelle Sri <michelle.sri@gmail.com>
2016-06-02 10:46:40 -0700

    found the chart that michael deleted

--- js/lang.js
+++ js/lang.js
@@ -119,3 +119,43 @@ function userLang (uCountry, uLanguage, visit, speaks){
 }
 
 // uVisited, uSpeaks uVisited.value, uSpeaks.value
+
+var canvas = document.getElementById('canvas');
+
+var countryNames = ['Russia', 'Turkey', 'Iran', 'Georgia', 'Armenia', 'Azerbaijan'];
+
+ // the number of speakers in the world is in the millions and corresponds to the order of the country names
+ // notes about the data (found on wikipedia.com):
+ // I used the total number of speakers for the data (L1 + L2)
+ // Russia: 150mil L1 speakers. 110mil L2 speakers
+ // Turkey: 71mil L1. 3mil L2
+ // Iran: 60mil L1. 50mil L2
+ // Georgia: 4.3mil total
+ // Armenia: 812mil total
+ // azerbaijan: 26mil total
+
+var speakersInWorld = ['260000000', '71000000', '110000000', '4300000', '12000000', '26000000'];
+
+function draw(countryNames, speakersInWorld) {
+
+  canvas.style.display = 'block';
+  var myChart = new Chart(canvas, {
+    type: 'bar',
+    data: {
+      labels: countryNames,
+      datasets: [{
+        label: 'Speakers in the World',
+        data: speakersInWorld,
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: { beginAtZero:true}
+        }]
+      }
+    }
+  });
+}
+
+draw(countryNames, speakersInWorld);
